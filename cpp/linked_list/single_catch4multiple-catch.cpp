4e47dec283e001882ec4ff86e87b5fa5d2df67dd
chombz <chombareuben@gmail.com>
2023-10-03 15:20:03 +0300

    linked list

---
+++ cpp/linked_list/single_catch4multiple-catch.cpp
@@ -0,0 +1,42 @@
+/* A C++ program to demonstrate Multiple catch blocks. */
+#include <iostream>
+using namespace std;
+
+// Multiple CATCH blocks for one TRY block
+void handler(int value)
+{
+    try
+    {
+        if (value)
+            {
+                cout << " Integer throw to proceed";
+                throw value;
+            }
+            else
+            {
+               cout << "String throw to proceed";
+                throw " Value passed to handler function is string";
+            }
+    }
+    catch (char *strin) //Catch block for zero value
+        {
+            cout << " A string exception caught: ";
+            cout << strin << "\n";
+        }
+    catch (int d) //Catch block for non-zero value
+        {
+            cout << " An integer exception caught\n";
+            cout << " The value is ";
+            cout << d << "\n";
+        }
+}
+
+//main method begins program execution
+int main()
+{
+    cout << " Start of program execution in main";
+    handler(1);
+    handler(0);
+    cout << "\n Program control back in main --- now it ends";
+    return 0;
+}
